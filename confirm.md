6320068879bb3beb67708befbfa579ab76ffe87b
Dave <davideweaver@hotmail.com>
2016-02-07 09:17:12 -0500

    updates and cname file

---
+++ confirm.md
@@ -0,0 +1,7 @@
+---
+layout: page
+title: Message Sent
+permalink: /confirm/
+---
+
+Your message was sent and I'll get back to you as soon as I can. Thanks.
---
+++ js/emailform.js
@@ -0,0 +1,15 @@
+/* global $ */
+
+(function() { 
+  $("#emailform").validate();
+	$( "#emailform" ).submit(function( event ) {	
+		if(!$("#name").val().length || !$("#email").val().length) {
+      $("#forminvalid").show();
+			event.preventDefault();
+      return;
+    }
+		$("#forminvalid").hide();
+    $("#submit").prop("disabled", true);
+    $("#spinner").show();
+	});
+}());
